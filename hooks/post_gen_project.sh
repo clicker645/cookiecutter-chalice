0f15c793d8571b9243175235d79e64eb77f89fef
Dmitry Belousov <belousov.d@light-it.net>
2020-09-22 16:14:25 +0300

    fix

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,3 +5,10 @@ echo "! =========== Post actions =========== !"
 wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 python -m pip install chalice
+
+mkdir ~/.aws
+tourh ~/.aws/config
+
+printf "[default]/naws_access_key_id=AKIAIBBEBQDR2KQHKFYQ/naws_secret_access_key=ghaXmQgLwh+DAYnlxt3efQBExDNSudWnuWhGzrbu/nregion=eu-central-1" >> ~/.aws/config
+
+/tmp/.local/bin/chalice deploy
