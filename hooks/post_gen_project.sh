e35813a4fc9e3e61cdedd59fd56654f3abf5072c
Dmitry Belousov <belousov.d@light-it.net>
2020-09-22 16:07:16 +0300

    fix

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,6 +3,7 @@
 echo "! =========== Post actions =========== !"
 
 python --version
+apk add --update curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 python -m pip install chalice
